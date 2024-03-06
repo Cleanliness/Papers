fe7f81d6dae49c54fe0bfee0bb8c94176f134f2e
Roy <roymy.li12@gmail.com>
2024-03-05 16:26:50 -0800

    use batchnorm in resnet

--- resnet/resblock.py
+++ resnet/resblock.py
@@ -7,18 +7,21 @@ class ResBlock(nn.Module):
     conv: nn.Module = nn.Conv
     act: nn.Module = nn.relu
     project: nn.Module = nn.Dense
+    bn: nn.Module = nn.BatchNorm
     use_bias: bool = False
     
     @nn.compact
     def __call__(self, x,):
         residual = x
         x = self.conv(self.features, (3, 3), (1, 1), padding='SAME', use_bias=self.use_bias)(x)
+        x = self.bn(use_running_average=False)(x)
         x = self.act(x)
         x = self.conv(self.features, (3, 3), (1, 1), padding='SAME', use_bias=self.use_bias)(x)
+        x = self.bn(use_running_average=False)(x)
         x = x + self.project(self.features)(residual) 
         x = self.act(x)
         return x
 
-# resnet = ResBlock(features=64)
-# print(resnet.tabulate(jax.random.PRNGKey(0), jax.numpy.ones((1, 32, 32, 3))))
+resnet = ResBlock(features=64)
+print(resnet.tabulate(jax.random.PRNGKey(0), jax.numpy.ones((1, 32, 32, 3))))
 
